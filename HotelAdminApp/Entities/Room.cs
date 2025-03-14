47ca7420c3aac283b39d6e3361989986bcb1b75f
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-03-14 14:00:09 +0100

    Added properties to class Room

--- HotelAdminApp/Entities/Room.cs
+++ HotelAdminApp/Entities/Room.cs
@@ -8,6 +8,12 @@ namespace HotelAdminApp.Entities
 {
     class Room
     {
+        public int RoomId { get; set; }//Primary Key
+        public string RoomNumber { get; set; } 
+        public string RoomType { get; set; } // Single/ Double
+        public int Capacity { get; set; } //Max number of guests allowed
+        public decimal PricePerNight { get; set; } //Max number of guests allowed
+        public List<Booking> Bookings { get; set; } // One room can have mutiple bookings over time (One to Many relationship)
 
     }
 }
