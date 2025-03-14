955e9b4e17e703c5bde74f6daa096c77eb75fcba
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-03-14 17:27:11 +0100

    Created a Build Method in Data initializer class  and created an instance of ApplicationDbContext using this method

--- HotelAdminApp/Data/DataInitializer.cs
+++ HotelAdminApp/Data/DataInitializer.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HotelAdminApp.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,32 @@ namespace HotelAdminApp.Data
 {
     class DataInitializer
     {
+
+        //This method creates and returns an instance of ApplicationDbContext
+        public static ApplicationDbContext Build()
+        {
+            //loading configuration from appsettings.json
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build(); //Build the configuration object
+
+            //Get the connection string from appsettings.json
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            //Create DbcOntextOptionsBuilder with SQL server
+            var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                                 .UseSqlServer(connectionString) 
+                                 .Options;
+
+            //Create an instance of ApplicationDbContext using configured options
+            var dbContext = new ApplicationDbContext(contextOptions);
+
+            //applying migrations 
+            dbContext.Database.Migrate();
+
+            //retuen the configured DbContext instance
+            return dbContext;
+        }
     }
 }
--- HotelAdminApp/Program.cs
+++ HotelAdminApp/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace HotelAdminApp
+﻿using HotelAdminApp.Data;
+
+namespace HotelAdminApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Creating an instance of ApplicationDbContext using DataInitializer.Build() method
+            using (var dbContext = DataInitializer.Build())
+            {
+               
+            }
         }
     }
 }
