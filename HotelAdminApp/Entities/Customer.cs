0de65fdadbe86bd55a409ebcd7d21eb8e0646383
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-03-14 14:08:17 +0100

    Wrote properties of class Customer

--- HotelAdminApp/Entities/Customer.cs
+++ HotelAdminApp/Entities/Customer.cs
@@ -8,5 +8,10 @@ namespace HotelAdminApp.Entities
 {
     class Customer
     {
+        public int CustomerId { get; set; } //Primary Key
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<Booking> Bookings { get; set; } //One customer can have multiple bookings over time (One to Many relationship)
     }
 }
