b736f44a021cefba4d33225fc643523a72da2a33
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-03-14 14:05:18 +0100

    Created a Booking class and wrote its properties

---
+++ HotelAdminApp/Entities/Booking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelAdminApp.Entities
+{
+    class Booking
+    {
+        public int BookingId { get; set; } //Primary Key
+        public Room Room { get; set; } //Many bookings can belong to one room over time (Many to One)
+        public Customer Customer { get; set; } //Many bookings can belong to one customer over time (Many to One)
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int NumberOfGuests { get; set; } //Number of guests for this booking should not exceed the room capacity
+        public Invoice Invoice { get; set; } //One booking will have one invoice (One to one relationship)
+
+    }
+}
