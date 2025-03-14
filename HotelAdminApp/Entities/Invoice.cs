f0f512b81968fe5b23c06a4859488a0faf7a083d
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-03-14 14:11:29 +0100

    Wrote properties of class Invoice

--- HotelAdminApp/Entities/Invoice.cs
+++ HotelAdminApp/Entities/Invoice.cs
@@ -8,5 +8,10 @@ namespace HotelAdminApp.Entities
 {
     class Invoice
     {
+        public int InvoiceId { get; set; } //Primary Key
+        public Booking Booking { get; set; }//One invoice will be with one booking (One to One relationship)
+        public decimal TotalAmount { get; set; }
+        public bool IsPaid { get; set; }
+        public DateTime DueDate { get; set; }
     }
 }
