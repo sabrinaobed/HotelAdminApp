35b76043751b81e52879fc7889ff56c37bbe1657
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-03-14 16:43:12 +0100

    Created DbSets to represent tables in database in DbContext

--- HotelAdminApp/Contexts/ApplicationDbContext.cs
+++ HotelAdminApp/Contexts/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HotelAdminApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,12 @@ using System.Threading.Tasks;
 
 namespace HotelAdminApp.Contexts
 {
-    class ApplicationDbContext
+    class ApplicationDbContext : DbContext
     {
+        //These DbSets represent the tables in the database 
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
     }
 }
