288a6bc79201f31c77d277a573952e7d135538b2
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-03-19 21:54:37 +0100

    method to get all bookings including customer and room

--- HotelAdminApp/Services/BookingService.cs
+++ HotelAdminApp/Services/BookingService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HotelAdminApp.Contexts;
+using HotelAdminApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,32 @@ namespace HotelAdminApp.Services
 {
    public  class BookingService
    {
-   }
+        private readonly ApplicationDbContext _dbContext; //_dbContext is a class variable
+
+
+        //intialized ApplicationDBContext via DI
+        public BookingService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext; //_dbContext is a class variable ans dbContext is a parameter
+        }
+
+
+        //-------------------CURD OPERATIONS------------------------
+
+                                            //Get all Bookings
+        public List<Booking> GetAllBookings()
+        {
+            return _dbContext.Bookings.Include(b => b.Customer).Include(b => b.Room).ToList(); //including customer and room as well in bookings
+        }
+
+                                               //Get a specific booking by its ID
+        public Booking? GetBookingById(int id)
+    
+    
+    
+    
+    
+    }
+
+
 }
