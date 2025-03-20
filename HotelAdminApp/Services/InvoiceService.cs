7fdf8253b6e1e48759bbfe7953b350673bd4ba23
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-03-20 10:50:31 +0100

    Created a constrcutor of class invoice service to intialize database via dependency injection

--- HotelAdminApp/Services/InvoiceService.cs
+++ HotelAdminApp/Services/InvoiceService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelAdminApp.Contexts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace HotelAdminApp.Services
 {
     public class InvoiceService
     {
+        //showing the reference to database context
+        private readonly ApplicationDbContext _dbContext; //_dbContext is a class variable
+
+        //This constructor intialized database context via DI and ensure we can interact with database without re-intializing it.
+        public InvoiceService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext; //_dbContext is a class variable and dbContext is a parameter
+        }
     }
 }
