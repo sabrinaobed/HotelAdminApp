93a37e3ea226dfb86b8f4285fee510702525bdb8
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-03-20 13:09:56 +0100

    Created constructor to intialize RoomService via DI

--- HotelAdminApp/Controllers/RoomController.cs
+++ HotelAdminApp/Controllers/RoomController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelAdminApp.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace HotelAdminApp.Controllers
 {
-    public class RoomController
+    public class RoomController //it handles user interactions for managing rooms and ensures data validation before callin RoomService
     {
+        private readonly RoomService _roomService; //created its class variable
+
+        //this constructor intializes room service via DI
+        public RoomController (RoomService roomService)
+        {
+            _roomService = roomService;
+        }
+
+
+        //Get all Rooms
+        public void GetAllRooms()
+        {
+            Console.Clear();
+        }
     }
 }
