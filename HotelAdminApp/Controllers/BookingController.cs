70d888b56c1aea3ea598807d5d8538b02835e22b
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-03-20 17:14:13 +0100

    Created constructor to intialize booking service via DI

--- HotelAdminApp/Controllers/BookingController.cs
+++ HotelAdminApp/Controllers/BookingController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelAdminApp.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,20 @@ namespace HotelAdminApp.Controllers
 {
     public class BookingController
     {
+        private readonly BookingService _bookingService;
+
+        //this constructor intializes  Booking service
+        public BookingController(BookingService bookingService)
+        {
+            _bookingService = bookingService;
+        }
+
+
+
+        //Get all bookings
+        public void GetAllBookings()
+        {
+
+        }
     }
 }
