f8ac0752955cdb84b6bc032867c75cab2417a685
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-03-20 17:36:29 +0100

    Created a method to fill details to add a new booking

--- HotelAdminApp/Controllers/BookingController.cs
+++ HotelAdminApp/Controllers/BookingController.cs
@@ -1,4 +1,5 @@
-﻿using HotelAdminApp.Services;
+﻿using HotelAdminApp.Entities;
+using HotelAdminApp.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -47,7 +48,7 @@ namespace HotelAdminApp.Controllers
         //Get booking by ID
         public void GetBookingById(int id)
         {
-            Console.WriteLine("Enter Booking ID: ");
+            Console.Write("Enter Booking ID: ");
             if(int.TryParse(Console.ReadLine(), out int bookingId))
             {
                 var booking = _bookingService.GetBookingById(bookingId);
@@ -65,6 +66,69 @@ namespace HotelAdminApp.Controllers
             {
                 Console.WriteLine("Inavlid ID. ");
             }
+        }
+
+
+
+
+
+
+        //Add a new booking
+        public void AddBooking()
+        {
+            Console.Clear();
+            Console.Write("Enter Customer ID: ");
+            if(!int.TryParse(Console.ReadLine(), out int customerId))
+            {
+                Console.WriteLine("Invalid Customer ID. ");
+                return;
+            }
+
+
+
+            Console.Write("Enter Room ID:  ");
+            if (!int.TryParse(Console.ReadLine(), out int roomId))
+            {
+                Console.WriteLine("Invalid Room ID. ");
+                return;
+            }
+
+            Console.Write("Enter Start Date(YYYY-MM-DD): ");
+            if(!DateTime.TryParse(Console.ReadLine(),out DateTime startDate) || startDate < DateTime.Today)
+            {
+                Console.WriteLine("Invalid start date.");
+                return;
+            }
+
+
+            Console.WriteLine("Enter End Date(YYYY-MM-DD): ");
+            if (!DateTime.TryParse(Console.ReadLine(), out DateTime endDate) || endDate <= startDate)
+            {
+                Console.WriteLine("Invalid end date.");
+                return;
+            }
+
+
+            Console.WriteLine("Enter number of Guests: ");
+            if(!int .TryParse(Console.ReadLine(),out int numberOfGuests) || numberOfGuests <= 0)
+            {
+                Console.WriteLine("invalid number of guests");
+                return;
+            }
+
+            var booking = new Booking
+            {
+                CustomerId = customerId,
+                RoomId = roomId,
+                StartDate = startDate,
+                EndDate = endDate,
+                NumberOfGuests = numberOfGuests
+            };
+
+            _bookingService.AddBooking(booking);
+            Console.WriteLine("Booking added successfully.");
+
+
         }
     }
 }
