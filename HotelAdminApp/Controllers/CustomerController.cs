57e9d8c635f9ac35ca495e56c60c859a20813ae8
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-03-20 14:33:09 +0100

    Created a method to get info about all customers

--- HotelAdminApp/Controllers/CustomerController.cs
+++ HotelAdminApp/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelAdminApp.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,32 @@ using System.Threading.Tasks;
 
 namespace HotelAdminApp.Controllers
 {
-    public  class CustomerController
+    public  class CustomerController //it handles user interactions for managing rooms and ensures data validation before callin RoomService
     {
+        private readonly CustomerService _customerService;
+
+        //this constructor intializes the customer service via DI
+        public CustomerController(CustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+
+        //Get all Customers
+        public void GetAllCustomers()
+        {
+            Console.Clear();
+            var customers = _customerService.GetAllCustomers();
+
+            if (customers.Count == 0)
+            {
+                Console.WriteLine("\nList of all Customers\n");
+                foreach(var customer in customers)
+                {
+                    Console.WriteLine($" ID: {customer.CustomerId}, CustomerName: {customer.Name}, Email: {customer.Email}, PhoneNumber: {customer.PhoneNumber}");
+                }
+            }
+        }
+
     }
 }
