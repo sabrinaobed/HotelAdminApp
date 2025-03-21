c6cdaaa18887236c15d3a69507bdaf1d7fd46275
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-03-21 15:46:58 +0100

    Created a method to get all invoices

--- HotelAdminApp/Controllers/InvoiceController.cs
+++ HotelAdminApp/Controllers/InvoiceController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HotelAdminApp.Services;
+using Microsoft.Identity.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,38 @@ namespace HotelAdminApp.Controllers
 {
     public class InvoiceController
     {
+        private readonly InvoiceService _invoiceService;
+
+        //this constructor intiliazes Invoices service via DI
+        public InvoiceController(InvoiceService invoiceService)
+        {
+            _invoiceService = invoiceService;
+        }
+
+
+        //Get all invoices
+        public void GetAllInvoices()
+        {
+            Console.Clear();
+            var invoices = _invoiceService.GetAllInvoices();
+
+            if(invoices.Count == 0)
+            {
+                Console.WriteLine("No invoices found");
+                return;
+            }
+            Console.WriteLine("\nList of Invoices: \n");
+            foreach( var invoice in invoices)
+            {
+                Console.WriteLine($"Invoice ID: {invoice.InvoiceId},Booking ID: {invoice.BookingId}, Amount: {invoice.TotalAmount}, Paid: {invoice.IsPaid}, Due: {invoice.DueDate}");
+            }
+        }
+
+
+
+
+
+
+        //Get invoice by ID
     }
 }
